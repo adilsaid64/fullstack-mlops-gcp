f763418123b1c1c713ffa53e2c228763df6c9ec6
adil <adilsaid64@gmail.com>
2025-04-07 00:39:55 +0300

    feat: terraform module for ecr

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,15 @@
+resource "aws_ecr_repository" "this" {
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = merge(
+    {
+      "Name" = var.ecr_repo_name
+    },
+    var.tags
+  )
+}
---
+++ terraform/modules/ecr/variables.tf
@@ -0,0 +1,10 @@
+variable "ecr_repo_name" {
+  description = "Name of ECR repo"
+
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
