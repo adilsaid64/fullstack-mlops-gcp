d7a551d6d62739d075365357c69e9c39e2462b3a
adil <adilsaid64@gmail.com>
2025-04-13 17:08:00 +0100

    feat: run type part of model name for easier lookups

--- pipelines/iris_pipeline.py
+++ pipelines/iris_pipeline.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from zenml import pipeline
 from steps.iris_pipeline_steps import load_data, train_model, evaluate_model
 from zenml.integrations.mlflow.steps.mlflow_registry import (
@@ -6,10 +7,13 @@ from zenml.integrations.mlflow.steps.mlflow_registry import (
 
 
 @pipeline  # type: ignore[misc]
-def iris_pipeline(run_type: str) -> None:
+def iris_pipeline(run_type: Literal["dev", "prod"]) -> None:
     """Basic pipeline for training an Iris classification model"""
     X_train, X_test, y_train, y_test = load_data()
     model = train_model(X_train, y_train)
-    mlflow_register_model_step(model=model, name="sklearn-iris-model")
+    mlflow_register_model_step(
+        model=model,
+        name=f"sklearn-iris-model-{run_type}",
+    )
     accuracy = evaluate_model(model, X_test, y_test)
     print(accuracy)
