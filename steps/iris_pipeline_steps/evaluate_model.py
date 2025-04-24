f717a47197e5c97c7a5f89ae5c518444b270d1b3
adil <adilsaid64@gmail.com>
2025-04-24 23:40:51 +0100

    docs: better typing

--- steps/iris_pipeline_steps/evaluate_model.py
+++ steps/iris_pipeline_steps/evaluate_model.py
@@ -6,6 +6,7 @@ import numpy as np
 import mlflow
 
 from typing import Annotated
+import numpy.typing as npt
 
 experiment_tracker = Client().active_stack.experiment_tracker
 
@@ -13,11 +14,12 @@ experiment_tracker = Client().active_stack.experiment_tracker
 @step(experiment_tracker=experiment_tracker.name)  # type: ignore[misc]
 def evaluate_model(
     model: LogisticRegression,
-    X_test: np.ndarray,
-    y_test: np.ndarray,
+    X_test: npt.NDArray[np.float64],
+    y_test: npt.NDArray[np.int64],
 ) -> Annotated[float, "accuracy"]:
-    predictions: np.ndarray = model.predict(X_test)
-    accuracy: float = accuracy_score(y_test, predictions)
+    """Evaluates model and returns accuracy"""
+    predictions: npt.NDArray[np.int64] = model.predict(X_test)
+    accuracy: float = float(accuracy_score(y_test, predictions))
     mlflow.log_metric("test_accuracy", accuracy)
     print(f"Model accuracy: {accuracy:.2f}")
     return accuracy
--- steps/iris_pipeline_steps/train_model.py
+++ steps/iris_pipeline_steps/train_model.py
@@ -4,21 +4,27 @@ from zenml.client import Client
 import numpy as np
 import mlflow
 from mlflow.models import infer_signature
+from mlflow.models.signature import ModelSignature
 from typing import Annotated
+import numpy.typing as npt
 
 experiment_tracker = Client().active_stack.experiment_tracker
 
 
 @step(experiment_tracker=experiment_tracker.name)  # type: ignore[misc]
 def train_model(
-    X_train: np.ndarray,
-    y_train: np.ndarray,
+    X_train: npt.NDArray[np.float64],
+    y_train: npt.NDArray[np.int64],
 ) -> Annotated[LogisticRegression, "trained_model"]:
+    """Trains and returns model"""
     mlflow.sklearn.autolog()
-    model = LogisticRegression(max_iter=200)
+    model: LogisticRegression = LogisticRegression(max_iter=200)
     model.fit(X_train, y_train)
 
-    signature = infer_signature(X_train, model.predict(X_train))
+    signature: ModelSignature = infer_signature(
+        X_train,
+        model.predict(X_train),
+    )
     mlflow.sklearn.log_model(model, "model", signature=signature)
 
     return model
